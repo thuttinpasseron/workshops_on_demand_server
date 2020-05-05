d39ecbbeccb8d7ea97b7b17f6b45b3ef69a8d195
pramod sareddy <reddypramod85>
2020-05-05 00:13:43 -0500

    :hammer:cronjob to end trail in 4 hours from start

--- modules/CheckCustomers/index.js
+++ modules/CheckCustomers/index.js
@@ -2,14 +2,41 @@ import cron from "cron";
 import models from "../../models";
 import createEmailBody from "../Email/createEmailBody";
 import sendEmail from "../Email";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const { CronJob } = cron;
 
-const getDaysLeft = ends => {
-  const oneDay = 24 * 60 * 60 * 1000;
+const getHoursLeft = ends => {
+  const oneHour = 1 * 60 * 60 * 1000;
   const endsDate = new Date(ends);
   const today = new Date();
-  return Math.round((endsDate.getTime() - today.getTime()) / oneDay);
+  return Math.round((endsDate.getTime() - today.getTime()) / oneHour);
+};
+
+const getDates = () => {
+  const startDate = new Date();
+  const endDate = new Date();
+  // const addHours = process.env.WORKSHOP_DURATION;
+  endDate.setHours(endDate.getHours() + 4);
+  return { startDate, endDate };
+};
+
+/* Function to generate combination of password */
+
+const generatePassword = () => {
+  var pass = "";
+  var str =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+
+  for (let i = 1; i <= 8; i++) {
+    var char = Math.floor(Math.random() * str.length + 1);
+
+    pass += str.charAt(char);
+  }
+
+  return pass;
 };
 
 export const monthNames = [
@@ -31,14 +58,12 @@ const checkCustomer = () => {
   models.customer
     .findAll({ include: [{ all: true, nested: true }] })
     .then(customers =>
-      customers.map(customer => {
+      customers.map(async customer => {
         // eslint-disable-line array-callback-return
         const { dataValues } = customer;
         const customerStatus = dataValues.active;
-        const endDate = new Date(dataValues.endDate);
-        const daysLeft = getDaysLeft(dataValues.endDate);
-        console.log("last email sent", dataValues.lastEmailSent);
-        console.log("student id check", dataValues.studentId);
+        //const updated = dataValues.upupdatedAt.getHours();
+        const hoursLeft = getHoursLeft(dataValues.endDate);
 
         // Send welcome email.
         if (!dataValues.lastEmailSent && dataValues.studentId != null) {
@@ -50,10 +75,10 @@ const checkCustomer = () => {
               heading: "Welcome to HPE Workshops On Demand!",
               content: `
                 Hi ${dataValues.name},</br>
-                Your request for the following workshop(s) has been received. We will send you the access details soon in a seperate email</br>
-                Workshop List: ${dataValues.workshopList}</br>
-                Start Date: ${dataValues.startDate}</br>
-                End Date: ${dataValues.endDate}
+                Your request for the <b>${dataValues.workshop}</b> workshop has been received. We will send you the access details soon in a seperate email.</br>
+                
+                <b>NOTE:</b> Your wokshop access will be expired in ${dataValues.hours} hours after you receive your credentials.</br>
+               
                 </br></br>
               `
             })
@@ -66,37 +91,40 @@ const checkCustomer = () => {
 
         // Send workshop credentilas as soon as there are ready.
         if (customerStatus && dataValues.lastEmailSent === "welcome") {
+          // fetch the customer requested workshop from workshops table
+          const workshop = await models.workshop.findOne({
+            where: { name: dataValues.workshop }
+          });
           console.log("send workshops credentials email");
           return sendEmail({
             recipient: dataValues.email,
             subject: "Your HPE Workshops On Demand credentials",
             content: createEmailBody({
               heading: "Your HPE Workshops On Demand credentials",
-              content: `Your access to the workshops will end on ${
-                monthNames[endDate.getMonth()]
-              } ${endDate.getDate()}, ${endDate.getFullYear()}.`,
-              buttonLabel: "View HPE Workshops",
+              content: `Your <b>${dataValues.workshop}</b> workshop credentials along with the video link are provided below to follow along the workshop. Your access to the workshop will end in ${dataValues.hours} hours from now.`,
+              buttonLabel: "Start Workshop",
               buttonUrl: dataValues.student.url,
               userName: dataValues.student.username,
-              password: dataValues.student.password
+              password: dataValues.student.password,
+              videoUrl: `${workshop.replayAvailable}` ? workshop.videoUrl : ""
             })
           }).then(() => {
             customer.update({
-              lastEmailSent: "credentials"
+              lastEmailSent: "credentials",
+              ...getDates()
             });
           });
         }
 
         // Send expired email.
-        if (daysLeft <= 0 && dataValues.lastEmailSent === "credentials") {
+        if (hoursLeft <= 0 && dataValues.lastEmailSent === "credentials") {
           console.log("send expired email");
           return sendEmail({
             recipient: dataValues.email,
             subject: "Your HPE Workshops On Demand trial has ended",
             content: createEmailBody({
               heading: "Thanks for trying HPE Workshops On Demand!",
-              content:
-                "We hope your enjoyed your HPE Workshops On Demand trial.",
+              content: `We hope you enjoyed <b>${dataValues.workshop}<b> Workshop On Demand trial.`,
               buttonLabel: "Click here to Provide the Feedback",
               buttonUrl:
                 "https://forms.office.com/Pages/ResponsePage.aspx?id=YSBbEGm2MUuSrCTTBNGV3KiKnXK8thhKg7iBfJh46UlUQzFEUUVGMVVQMEowMElUMVY3WkVUU0pWVi4u"
@@ -107,7 +135,8 @@ const checkCustomer = () => {
               active: false
             });
             customer.student.update({
-              assigned: false
+              assigned: false,
+              password: generatePassword()
             });
           });
         }
