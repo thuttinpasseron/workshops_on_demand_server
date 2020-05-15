ae5de24422bd5dbba867860ca2a4d908605c3b6c
pramod sareddy <reddypramod85>
2020-05-15 00:53:25 -0500

    update notebook url

--- seeders/02-students.js
+++ seeders/02-students.js
@@ -1,12 +1,12 @@
 module.exports = {
   up: queryInterface => {
-    const N = 3;
+    const N = 10;
     const arr = [...Array(N + 1).keys()].slice(1);
     const entries = arr.map(key => ({
       createdAt: new Date(),
       updatedAt: new Date(),
       //   customerId: 0,
-      url: `https://notebooks.hpedev.io:8000/user/student${key}/lab`,
+      url: `https://notebooks.hpedev.io/user/student${key}/lab?`,
       username: `student${key}`,
       password: "MyNewPassword"
     }));
