d82c9a2fea44cab6474d809a7b59d0cad5ea6a32
Johnathan Giang <jayhgiang@gmail.com>
2021-04-16 11:51:44 -0700

    Create api GET route and swagger

--- models/replay.js
+++ models/replay.js
@@ -7,7 +7,12 @@
  *      Replay:
  *        type: object
  *        required:
- *          - name
+ *          - avatar
+ *          - title
+ *          - desc
+ *          - presenter
+ *          - role
+ *          - videoLink
  *        properties:
  *          id:
  *            type: integer
@@ -22,7 +27,8 @@
  *            type: string
  *          role:
  *            type: string
- *          videoLink: string
+ *          videoLink: 
+ *            type: string
  *          createdAt:
  *            type: string
  *            format: date-time
@@ -35,7 +41,7 @@
  *           desc: Calling all developers, data analysts and IT architects. Learn how to interact with the HPE Ezmeral Container Platform programmatically through its REST API. In this hands-on workshop, you'll learn how to perform authentication, deploy distributed multi-mode applications for AI/ML and data analytics and interpret/respond to REST API calls. We'll walk you through, step by step, and, by the end of the session, you'll be deploying a TensorFlow application framework and other simple applications. No previous programming experience required.
  *           presenter: Denis Choukroun
  *           role: Cloud Solution Architect
- *           videolink: https://vimeo.com/445302013
+ *           videoLink: https://vimeo.com/445302013
  */
 module.exports = (sequelize, DataTypes) => {
   const Replay = sequelize.define(
