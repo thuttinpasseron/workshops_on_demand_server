2e56cf472a3f6e63837fc689336f8dafeb19e671
Johnathan Giang <jayhgiang@gmail.com>
2021-06-09 23:06:47 -0700

    Add migration and seeders for specialBadgeId colun and special_badges table

---
+++ seeders/07-special_badges.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface) =>
+    queryInterface.bulkInsert(
+      'special_badges',
+      [
+        {
+          id: 1,
+          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/explorer.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/expert.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 3,
+          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/star.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 4,
+          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/superstar.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+      }
+    ),
+
+  down: (queryInterface) => queryInterface.bulkDelete('special_badges', null, {}),
+};
