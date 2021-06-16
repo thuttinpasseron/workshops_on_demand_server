9c91ba24317f047b68e7674683731e1a42e2dc80
Johnathan Giang <jayhgiang@gmail.com>
2021-06-16 15:56:44 -0700

    Update special badge seeder images

--- seeders/07-special_badges.js
+++ seeders/07-special_badges.js
@@ -5,25 +5,25 @@ module.exports = {
       [
         {
           id: 1,
-          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/explorer.jpg',
+          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/3_workshops_apprentice.jpg',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           id: 2,
-          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/expert.jpg',
+          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/5_workshops_pathfinder.jpg',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           id: 3,
-          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/star.jpg',
+          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/7_workshops_squire.jpg',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           id: 4,
-          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/superstar.jpg',
+          badgeImg: 'https://us-central1-grommet-designer.cloudfunctions.net/images/jay-giang-hpe-com/10_workshops_adventurer.jpg',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
